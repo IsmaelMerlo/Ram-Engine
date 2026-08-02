0407e9a775ab706b3618898a391cf0110ed4c19f
Ismael Merlo (Viremorfe) <ismaelmerlo05@gmail.com>
2026-08-02 19:20:36 -0400

    Editor.cpp and Editor.h added to the project and fixed in the CMakeLists.txt

---
+++ include/Editor.h
@@ -0,0 +1,8 @@
+//
+// Created by viremorfe on 2/8/26.
+//
+
+#ifndef RAM_ENGINE_EDITOR_H
+#define RAM_ENGINE_EDITOR_H
+
+#endif //RAM_ENGINE_EDITOR_H
---
+++ src/Editor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by viremorfe on 2/8/26.
+//
+
+#include "../include/Editor.h"
